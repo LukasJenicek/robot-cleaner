4f8151f4575bc5ffe41694890eafbdb02af8a619
Lukas Jenicek <lukas@spendee.com>
2019-12-15 17:42:42 +0100

    Create entrypoint to application

---
+++ cleaning_robot.php
@@ -0,0 +1,50 @@
+<?php
+
+use CleaningRobot\CleaningService;
+use CleaningRobot\Robot\BackOffStrategy;
+use CleaningRobot\Robot\Factory\AdvanceFactory;
+use CleaningRobot\Robot\Factory\BackFactory;
+use CleaningRobot\Robot\Factory\TurnLeftFactory;
+use CleaningRobot\Robot\Factory\TurnRightFactory;
+use Psr\Log\Test\TestLogger;
+
+require_once 'vendor/autoload.php';
+
+if (!isset($argv[1])) {
+    throw new InvalidArgumentException('Input json is mandatory argument');
+}
+
+if (!isset($argv[2])) {
+    throw new InvalidArgumentException('Output file name is mandatory argument');
+}
+
+$sourceFile          = $argv[1];
+$destinationFileName = $argv[2];
+
+if (file_exists($sourceFile) === false) {
+    throw new InvalidArgumentException(
+        sprintf('File %s was not found', $sourceFile)
+    );
+}
+
+$input = json_decode(file_get_contents($sourceFile), true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    throw new InvalidArgumentException(
+        'JSON in passed file is invalid'
+    );
+}
+
+$logger = new TestLogger();
+
+$cleaningService = new CleaningService(
+    new BackOffStrategy(
+        new AdvanceFactory($logger),
+        new BackFactory($logger),
+        new TurnLeftFactory($logger),
+        new TurnRightFactory($logger),
+        $logger
+    ),
+    $logger
+);
+
